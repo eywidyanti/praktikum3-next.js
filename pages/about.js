2bc0ae02c66b2e56469031e5ada5b2bce7293eba
Eka <eywidyanti@email.com>
2025-02-27 22:28:23 +0700

    next.js

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>
+                Tentang Kami
+            </h1>
+            <p>Kami adalah perusahaan yang mengkhususkan diri dalam pembuatan website berkualitas tinggi.</p>
+        </div>
+    );
+};
+
+export default AboutPage;
